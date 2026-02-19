2eeba25d5b84053debaf215b33d9ad6b16f991ee
Xavier Rincon <xavirr1994@gmail.com>
2026-02-19 16:46:02 +0100

    dev tools

--- src/components/providers/query-provider.tsx
+++ src/components/providers/query-provider.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
@@ -16,5 +17,10 @@ export function QueryProvider({ children }: { children: React.ReactNode }) {
       })
   )
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
+    </QueryClientProvider>
+  )
 }
