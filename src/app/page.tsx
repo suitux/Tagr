d5f24e42184eb619b26af66b655144b4a5032851
Xavier Rincon <xavirr1994@gmail.com>
2026-02-19 12:49:09 +0100

    added button and shadcn0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -58,6 +59,8 @@ export default function Home() {
           >
             Documentation
           </a>
+
+            <Button>HOLA KE ASE</Button>
         </div>
       </main>
     </div>
