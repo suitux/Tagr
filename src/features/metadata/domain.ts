e3ccc3002acbecc4de4b70497434eaa4efecd3a2
Xavier Rincon <xavirr1994@gmail.com>
2026-02-20 22:55:45 +0100

    Move db

---
+++ src/features/metadata/domain.ts
@@ -0,0 +1,16 @@
+export interface SongMetadataUpdate {
+  title?: string
+  artist?: string
+  album?: string
+  albumArtist?: string
+  year?: number
+  trackNumber?: number
+  trackTotal?: number
+  discNumber?: number
+  discTotal?: number
+  genre?: string
+  composer?: string
+  comment?: string
+  lyrics?: string
+  bpm?: number
+}
