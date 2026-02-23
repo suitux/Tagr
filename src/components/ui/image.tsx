19ef7fefba8b418d9475a8fb65f1cf6b7f9344b0
Xavier Rincon <xavirr1994@gmail.com>
2026-02-23 20:23:38 +0100

    fix image

--- src/components/ui/image.tsx
+++ src/components/ui/image.tsx
@@ -1,19 +1,31 @@
 import { useState } from 'react'
 import NextImage, { ImageProps } from 'next/image'
 
+const failedSrcs = new Set<string>()
+
 interface ImageWithFallbackProps extends Omit<ImageProps, 'src'> {
   fallbackComponent?: React.ReactNode
   src?: string
 }
 
 const Image = ({ fallbackComponent, alt, src = '', ...props }: ImageWithFallbackProps) => {
-  const [error, setError] = useState<boolean>(false)
+  const [error, setError] = useState<boolean>(() => failedSrcs.has(src))
 
   if (error) {
     return fallbackComponent
   }
 
-  return <NextImage alt={alt} src={src} onError={() => setError(true)} {...props} />
+  return (
+    <NextImage
+      alt={alt}
+      src={src}
+      onError={() => {
+        failedSrcs.add(src)
+        setError(true)
+      }}
+      {...props}
+    />
+  )
 }
 
 export { Image }
