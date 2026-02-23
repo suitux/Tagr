7e0452c8b57f5e33051f85df446170650df7ea3b
Xavier Rincon <xavirr1994@gmail.com>
2026-02-23 15:54:49 +0100

    new fields

--- src/features/metadata/metadata-write.service.ts
+++ src/features/metadata/metadata-write.service.ts
@@ -15,16 +15,16 @@ export async function writeMetadataToFile(filePath: string, metadata: SongMetada
       tag.performers = metadata.artist ? [metadata.artist] : []
     }
 
+    if (metadata.sortArtist !== undefined) {
+      tag.performersSort = metadata.sortArtist ? [metadata.sortArtist] : []
+    }
+
     if (metadata.album !== undefined) {
       tag.album = metadata.album
     }
 
-    if (metadata.albumArtist !== undefined) {
-      tag.albumArtists = metadata.albumArtist ? [metadata.albumArtist] : []
-    }
-
-    if (metadata.year !== undefined) {
-      tag.year = metadata.year
+    if (metadata.sortAlbum !== undefined) {
+      tag.albumSort = metadata.sortAlbum
     }
 
     if (metadata.trackNumber !== undefined) {
@@ -43,24 +43,64 @@ export async function writeMetadataToFile(filePath: string, metadata: SongMetada
       tag.discCount = metadata.discTotal
     }
 
+    if (metadata.year !== undefined) {
+      tag.year = metadata.year
+    }
+
+    if (metadata.bpm !== undefined) {
+      tag.beatsPerMinute = metadata.bpm
+    }
+
     if (metadata.genre !== undefined) {
       tag.genres = metadata.genre ? [metadata.genre] : []
+    }
+
+    if (metadata.albumArtist !== undefined) {
+      tag.albumArtists = metadata.albumArtist ? [metadata.albumArtist] : []
+    }
+
+    if (metadata.sortAlbumArtist !== undefined) {
+      tag.albumArtistsSort = metadata.sortAlbumArtist ? [metadata.sortAlbumArtist] : []
     }
 
     if (metadata.composer !== undefined) {
       tag.composers = metadata.composer ? [metadata.composer] : []
     }
 
+    if (metadata.conductor !== undefined) {
+      tag.conductor = metadata.conductor
+    }
+
     if (metadata.comment !== undefined) {
       tag.comment = metadata.comment
+    }
+
+    if (metadata.grouping !== undefined) {
+      tag.grouping = metadata.grouping
+    }
+
+    if (metadata.publisher !== undefined) {
+      tag.publisher = metadata.publisher
+    }
+
+    if (metadata.description !== undefined) {
+      tag.description = metadata.description
+    }
+
+    if (metadata.discSubtitle !== undefined) {
+      tag.subtitle = metadata.discSubtitle
+    }
+
+    if (metadata.copyright !== undefined) {
+      tag.copyright = metadata.copyright
     }
 
     if (metadata.lyrics !== undefined) {
       tag.lyrics = metadata.lyrics
     }
 
-    if (metadata.bpm !== undefined) {
-      tag.beatsPerMinute = metadata.bpm
+    if (metadata.compilation !== undefined) {
+      tag.isCompilation = metadata.compilation
     }
 
     file.save()
