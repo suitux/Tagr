a178c8f92e4271be820cc39e0167b370e3b89a58
Xavier Rincon <xavirr1994@gmail.com>
2026-02-27 20:21:15 +0100

    import from alias

--- src/infrastructure/prisma/dbClient.ts
+++ src/infrastructure/prisma/dbClient.ts
@@ -1,5 +1,5 @@
+import { PrismaClient } from '@/generated/prisma/client'
 import { PrismaLibSql } from '@prisma/adapter-libsql'
-import { PrismaClient } from '../../generated/prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
